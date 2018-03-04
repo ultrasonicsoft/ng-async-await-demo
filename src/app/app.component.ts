09c3c30bbcd8b5d2a097d5a48439d01bb53e7c3b
ultrasonicsoft <balramchavan@gmail.com>
2018-03-04 12:47:36 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,69 @@ export class AppComponent {
   promiseResult: Employee;
   asyncResult: Employee;
 
+  conditionalPromiseResult: Employee;
+  conditionalAsyncResult: Employee;
+
+  additionPromiseResult: number;
+  additionAsyncResult: number;
+
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit() {
+    this.getValueWithPromise();
+    this.getValueWithAsync();
+
+    this.addWithPromise();
+    this.addWithAsync();
+
     this.getDataUsingSubscribe();
     this.getDataUsingPromise();
     this.getAsyncData();
+
+    this.getConditionalDataUsingPromise();
+    this.getConditionalDataUsingAsync();
+
+
+  }
+
+  resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 2000);
+    });
+  }
+
+  getValueWithPromise() {
+    this.resolveAfter2Seconds(20).then(value => {
+      console.log(`promise result: ${value}`);
+    });
+    console.log('I will not wait until promise is resolved');
+  }
+
+  async getValueWithAsync() {
+    const value = <number>await this.resolveAfter2Seconds(20);
+    console.log(`async result: ${value}`);
+  }
+
+
+  addWithPromise() {
+    this.resolveAfter2Seconds(20).then(data1 => {
+      let result1 = <number>data1;
+      this.resolveAfter2Seconds(30).then(data2 => {
+        let result2 = <number>data2;
+        this.additionPromiseResult = result1 + result2;
+        console.log(`promise result: ${this.additionPromiseResult}`);
+      });
+    });
+  }
+
+  async addWithAsync() {
+    const result1 = <number>await this.resolveAfter2Seconds(20);
+    const result2 = <number>await this.resolveAfter2Seconds(30);
+    this.additionAsyncResult = result1 + result2;
+    console.log(`async result: ${this.additionAsyncResult}`);
   }
 
   getDataUsingSubscribe() {
@@ -42,6 +98,28 @@ export class AppComponent {
 
   async getAsyncData() {
     this.asyncResult = await this.httpClient.get<Employee>(this.url).toPromise();
-    console.log('No issues, I will wait until promise is resolved..');    
+    console.log('No issues, I will wait until promise is resolved..');
+  }
+
+  getConditionalDataUsingPromise() {
+    this.httpClient.get<Employee>(this.url).toPromise().then(data => {
+      console.log('First Promise resolved.')
+      if (data.id > 5) {
+        let anotherUrl = 'http://dummy.restapiexample.com/api/v1/employee/23';
+        this.httpClient.get<Employee>(anotherUrl).toPromise().then(data => {
+          this.conditionalPromiseResult = data;
+          console.log('Second Promise resolved.')
+        });
+      }
+    });
+  }
+
+  async getConditionalDataUsingAsync() {
+    let data = await this.httpClient.get<Employee>(this.url).toPromise();
+    if (data.id > 5) {
+      let anotherUrl = 'http://dummy.restapiexample.com/api/v1/employee/23';
+      this.conditionalAsyncResult = await this.httpClient.get<Employee>(anotherUrl).toPromise();
+    }
+    console.log('No issues, I will wait until promise is resolved..');
   }
 }
